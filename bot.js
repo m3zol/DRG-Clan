d397255789d20f05616a44df587ff8a3920fcda9
DRG-Clan <52467881+DRG-Clan@users.noreply.github.com>
2019-07-03 10:24:58 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -105,13 +105,17 @@ var now_playing = [];
 \\\\\\\\\\\\\\\\\\\\\\\\/////////////////////////
 */
 //
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  if (msg.content === 'باك') {
+    msg.reply('ولكم');
   }
 });
-//
 
-
-
-client.login(process.env.BOT_TOKEN);
+client.login('token');
